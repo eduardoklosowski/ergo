ee6f14e9c561a30fd091d237461d8025bb86368b
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-03-25 11:30:29 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+version = __import__('ergo').__version__
+with open('README.rst', 'rb') as f:
+    long_description = f.read().decode('utf-8')
+
+
+setup(
+    name='ergo',
+    version=version,
+    packages=find_packages(),
+
+    install_requires=['Django>=1.8c1'],
+
+    author='Eduardo Augusto Klosowski',
+    author_email='eduardo_klosowski@yahoo.com',
+
+    description='',
+    long_description=long_description,
+    license='MIT',
+    url='https://github.com/eduardoklosowski/ergo',
+
+    include_package_data=True,
+    zip_safe=False,
+)
