ff14f26a81493a04cbb3be3461760d02ab71dc1c
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-03-27 15:01:12 -0300

    Add foundation fieldsets

--- ergo/foundation/forms.py
+++ ergo/foundation/forms.py
@@ -12,3 +12,10 @@ class FoundationForm(object):
     def foundation_column_class(self, field):
         columns = getattr(self.Meta, 'foundation_column_class', {})
         return columns.get(field, 'small-12 medium-6') + ' columns'
+
+    def foundation_fieldsets(self):
+        if hasattr(self.Meta, 'foundation_fieldsets'):
+            return [(name, [self[field] for field in fields])
+                    for name, fields in self.Meta.foundation_fieldsets]
+
+        return ((None, self.visible_fields()),)
