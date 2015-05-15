8772657110431f74284462a86972971baea60998
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-05-15 16:47:57 -0300

    Criado setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+version = __import__('ergo').__version__
+with open('README.rst', 'rb') as f:
+    long_description = f.read().decode('utf-8')
+
+
+setup(
+    name='ergo',
+    version=version,
+    packages=find_packages(),
+
+    install_requires=[
+        'Django>=1.8,<1.9',
+    ],
+
+    author='Eduardo Augusto Klosowski',
+    author_email='eduardo_klosowski@yahoo.com',
+
+    description='Core do projeto Ergo',
+    long_description=long_description,
+    license='AGPLv3+',
+    url='https://github.com/eduardoklosowski/ergo',
+
+    include_package_data=True,
+    zip_safe=False,
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
