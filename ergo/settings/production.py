51e807ab7e0050fa0108cf096eadd335d880b500
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2015-05-29 13:59:29 -0300

    Adicionado cache nas sessões em produção

--- ergo/settings/production.py
+++ ergo/settings/production.py
@@ -29,3 +29,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Optimizations
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
